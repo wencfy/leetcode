4f49387af0af7c6d6231cd0452bf1de9226d4062
wen <wen.wa@qq.com>
2022-11-12 23:27:11 +0800

    feat: (src) Day 13: dynamic programming, greedy, jump game, after optimize

--- src/0055.cpp
+++ src/0055.cpp
@@ -12,31 +12,20 @@ using namespace std;
 
 class Solution {
 public:
-    vector<bool> reachTable;
-
     bool canJump(vector<int>& nums) {
-        reachTable.resize(nums.size());
-        reachTable[0] = true;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (reachTable[i]) {
-                for (int j = 1; j <= nums[i] && i + j < nums.size(); j++) {
-                    reachTable[i + j] = true;
-                }
+        int neededSteps = 1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] >= neededSteps) {
+                neededSteps = 0;
             }
+            neededSteps++;
         }
-
-        // for (auto i: reachTable) {
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-
-        return reachTable[nums.size() - 1];
+        return neededSteps == 1;
     }
 };
 
 int main() {
-    // vector<int> nums = { 3, 2, 1, 0, 4 };
-    vector<int> nums = { 2, 3, 1, 1, 4 };
+    vector<int> nums = { 3, 2, 1, 0, 4 };
+    // vector<int> nums = { 2, 3, 1, 1, 4 };
     cout << (new Solution()) ->canJump(nums) << endl;
 }
