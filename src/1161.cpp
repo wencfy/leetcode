be1e93925428cbdab61988e522dbeb4f54604f76
wen <wen.wa@qq.com>
2023-06-17 23:36:29 +0800

    feat: (src) Daily Quiz: Maximum Level Sum of a Binary Tree

---
+++ src/1161.cpp
@@ -0,0 +1,65 @@
+/**
+ * Maximum Level Sum of a Binary Tree
+ * 
+ * Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+ * Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+ */
+
+#include "iostream"
+#include "limits.h"
+#include "queue"
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<pair<int, TreeNode*>> q;
+
+        int maxSum = INT_MIN;
+        int maxLevel;
+        int levelSum = 0;
+        int curLevel = 1;
+
+        // {level, value}
+        q.push({1, root});
+        while (!q.empty()) {
+            auto levelNodePair = q.front();
+            q.pop();
+            if (curLevel == levelNodePair.first) {
+                levelSum += levelNodePair.second->val;
+            } else {
+                if (levelSum > maxSum) {
+                    cout << curLevel << ": " << levelSum << " " << maxSum << endl;
+                    maxSum = levelSum;
+                    maxLevel = curLevel;
+                }
+                levelSum = levelNodePair.second->val;
+                curLevel = levelNodePair.first;
+            }
+
+            if (levelNodePair.second->left) {
+                q.push({levelNodePair.first + 1, levelNodePair.second->left});
+            }
+            if (levelNodePair.second->right) {
+                q.push({levelNodePair.first + 1, levelNodePair.second->right});
+            }
+        }
+
+        if (levelSum > maxSum) {
+            maxSum = levelSum;
+            maxLevel = curLevel;
+        }
+
+        return maxLevel;
+    }
+};
