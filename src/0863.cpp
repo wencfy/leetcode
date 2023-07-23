bce559b66656a65f17c0dfa18574ab4f3f83d9ae
wen <wen.wa@qq.com>
2023-07-23 13:25:08 +0800

    feat: add Tree.h and Tree.cpp to utils

--- src/0863.cpp
+++ src/0863.cpp
@@ -10,14 +10,8 @@
 #include "vector"
 #include "queue"
 #include "map"
+#include "utils/Tree.h"
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 class Solution {
     map<TreeNode *, TreeNode *> fa;
---
+++ src/utils/Tree.h
@@ -0,0 +1,8 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode();
+    TreeNode(int x);
+    TreeNode(int x, TreeNode *left, TreeNode *right);
+};
