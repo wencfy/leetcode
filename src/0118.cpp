e3f3d16792260d8ee8b2324562da2f2cbe2a2312
wen <wen.wa@qq.com>
2023-09-07 21:29:29 -0400

    feat: (src) Daily Quiz: Pascal's Triangle

---
+++ src/0118.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret = vector<vector<int>>(numRows, vector<int>());
+        ret[0].push_back(1);
+        for (int i = 1; i < numRows; i++) {
+            ret[i].push_back(1);
+            for (int j = 1; j < i; j++) {
+                ret[i].push_back(ret[i - 1][j - 1] + ret[i - 1][j]);
+            }
+            ret[i].push_back(1);
+        }
+        return ret;
+    }
+};
